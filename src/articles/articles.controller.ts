f1a421f5bde7b9bd6098059a92699a91575c2df7
Edu <eedumarquess@gmail.com>
2024-05-15 00:00:00 -0300

    FEAT: create filter for prisma exceptions

--- src/articles/articles.controller.ts
+++ src/articles/articles.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, ParseIntPipe } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, ParseIntPipe, NotFoundException } from '@nestjs/common';
 import { ArticlesService } from './articles.service';
 import { CreateArticleDto } from './dto/create-article.dto';
 import { UpdateArticleDto } from './dto/update-article.dto';
@@ -31,7 +31,13 @@ export class ArticlesController {
   @Get(':id')
   @ApiOkResponse({ type: ArticleEntity })
   findOne(@Param('id', ParseIntPipe) id: number) {
-    return this.articlesService.findOne(id);
+    const article = this.articlesService.findOne(id);
+
+    if (!article) {
+      throw new NotFoundException(`Article with ${id} does not exist.`)
+    }
+
+    return article;
   }
 
   @Patch(':id')
