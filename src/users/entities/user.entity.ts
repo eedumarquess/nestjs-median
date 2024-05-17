89866dc0759950b3f0060a0edb3f30a93234ca41
Edu <eedumarquess@gmail.com>
2024-05-17 00:00:00 -0300

    FEAT: update user entity and create/update DTO

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,21 @@
-export class User {}
+import { User } from "@prisma/client";
+import { ApiProperty } from "@nestjs/swagger";
+
+export class UserEntity implements User {
+  @ApiProperty()
+  id: number;
+
+  @ApiProperty()
+  createdAt: Date;
+
+  @ApiProperty()
+  updatedAt: Date;
+
+  @ApiProperty()
+  name: string;
+
+  @ApiProperty()
+  email: string;
+
+  password: string;
+}
